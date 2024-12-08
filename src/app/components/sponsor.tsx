367af22c6e62363d206f1d9b8c65d2d7a72d3b41
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-12-09 00:25:26 +0500

    update code

--- src/app/components/sponsor.tsx
+++ src/app/components/sponsor.tsx
@@ -5,7 +5,7 @@ const Sponsor = (props: any) => {
   return (
     <div>
         <div className='mx-2 my-4 sm:mx-8 sm:my-6 md:mx-6 md:my-12'>
-            <Image className=' w-[100px] h-[139px]' src={props.image} alt={props.alt} width={500} height={500}></Image>
+            <Image className=' w-full h-full' src={props.image} alt={props.alt} width={500} height={500}></Image>
         </div>
     </div>
   )
